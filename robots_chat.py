0faa3737f1cab8e1c5ecd33da21c803691a9af08
pokerstarxy <857607332@qq.com>
2017-06-02 18:28:06 +0800

    go on

--- robots_chat.py
+++ robots_chat.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 #coding=utf8
-import  itchat,re,time,requests,smtplib,json
+import  itchat,re,time,requests,smtplib,json,os
 from itchat.content import *
-from email import encoders
-from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
-
 
 
 def tuling_api(info):
@@ -18,6 +15,8 @@ def tuling_api(info):
         'userid':'webchat',
     }
     return requests.post(api_url,data=data)
+
+
 def mailme():
     subject_cont = u"温馨提示--关于微信错误提示"
     cont = u"robots wrong！"
@@ -31,62 +30,57 @@ def mailme():
     server.quit()
 
 
-def main():
-    @itchat.msg_register([TEXT, PICTURE, MAP, CARD, NOTE, SHARING,RECORDING, ATTACHMENT, VIDEO])
-    def text_reply(msg):
-        friend = itchat.search_friends(userName=msg['FromUserName'])
-        if msg['Type'] == 'Text' :
-            if friend['NickName']!='qwert':
-                reply_content=msg['Text']
-                try:
-                    reply_bak = tuling_api(reply_content).text
-                    reply_bak=json.loads(reply_bak)
-                except Exception:
-                    reply_bak = u'网络不好哦'
-                    mailme()
-        elif msg['Type'] == 'Picture':
-            reply_content = u"图片: " + msg['FileName']
-        elif msg['Type'] == 'Card':
-            reply_content = u" " + msg['RecommendInfo']['NickName'] + u" 的名片"
-        elif msg['Type'] == 'Map':
-            x, y, location = re.search("<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*", msg['OriContent']).group(
-                1,
-                2,
-                3)
-            if location is None:
-                reply_content = u"位置: 纬度->" + x.__str__() + u" 经度->" + y.__str__()
-            else:
-                reply_content = u"位置: " + location
-        elif msg['Type'] == 'Note':
-            reply_content = u"通知"
-        elif msg['Type'] == 'Sharing':
-            reply_content = u"分享"
-        elif msg['Type'] == 'Recording':
-            reply_content = u"语音"
-        elif msg['Type'] == 'Attachment':
-            reply_content = u"文件: " + msg['FileName']
-        elif msg['Type'] == 'Video':
-            reply_content = u"视频: " + msg['FileName']
-        else:
-            reply_content = u"你发的是什么科技啊！"
-        if friend['NickName']!='qwert':
-            if reply_content!="<Response [200]>":
-                itchat.send(u"Friend:%s -- %s    "
-                            u"Time:%s    "
-                            u" Message:%s" % (friend['NickName'], friend['RemarkName'], time.ctime(), reply_content),
-                            toUserName='filehelper')
-            if msg['Type']!='Text':
-                itchat.send(u"我已经收到你在【%s】发送的消息,稍后回复。--微信助手" % (time.ctime(), ),
-                            toUserName=msg['FromUserName'])
-            else:
-                results=''
+@itchat.msg_register([TEXT, PICTURE, MAP, CARD, NOTE, SHARING,RECORDING, ATTACHMENT, VIDEO])
+def text_reply(msg):
+    friend = itchat.search_friends(userName=msg['FromUserName'])
+    print friend['NickName']
+    if msg['Type'] == 'Text' :
+        receive_cont=msg['Text']
+        msg_info = "%s:  %s--%s " % (time.ctime(), msg['NickName'], receive_cont)
+        msg_store(msg_info)
+        try:
+            reply_content = tuling_api(receive_cont).text
+        except Exception as e:
+            reply_content = {u'text':u'机器人出了故障哦', u'code':u'error'}
+            mailme()
+    else:
+        reply_content = {u'text':u'你发的是什么科技啊！',u'code':u'error'}
+    reply_content = json.loads(reply_content)
+    results = ''
+    del reply_content[u'code']
+    ##多层次嵌套字典处理
+    results='\n'.join(reply_content.values())
+    itchat.send(u"Friend:%s -- %s    "
+                u"Time:%s    "
+                u" Message:%s" % (friend['NickName'], friend['RemarkName'], time.ctime(), receive_cont),
+                toUserName='filehelper')
 
-                del reply_bak[u'code']
-                for p,q in reply_bak.items():
-                    results=results+"%s:%s\n" %(p,q)
-                itchat.send(results,toUserName=msg['FromUserName'])
-    itchat.auto_login(hotReload=True)
-    itchat.run()
+    itchat.send(results, toUserName=msg['FromUserName'])
+    # itchat.send(u"我已经收到你在【%s】发送的消息,稍后回复。--微信助手" % (time.ctime(),),toUserName=msg['FromUserName'])
+    #                     toUserName=msg['FromUserName'])
+
+
+def msg_back(reply,msg):
+    pass
+
+
+def trans_photos():
+    path=os.path.join(os.path.dirname("__file__"),'itchat.pkl')
+    with open (path,'rb+') as f:
+        cont=f.read()
+
+def msg_store(msg):
+    with open('msg.bak','a+') as f:
+
+        f.write(msg)
+
+
+def main():
+    try:
+        itchat.auto_login(hotReload=True)
+        itchat.run()
+    except ConnectionError as e:
+        print e.message
 
 
 if __name__ == "__main__":
